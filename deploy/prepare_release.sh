87144830e3723517074688caf5978b44d5f25634
Édouard Lopez <contact@edouard-lopez.com>
2016-06-27 18:00:14 +0200

    add deploy scripts

---
+++ deploy/prepare_release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "prepare release"
+cd ..
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml rm -f
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+echo "run tests"
+docker exec coaxisopt_frontend_1  npm run test
+docker exec coaxisopt_backend_1 python manage.py test
+
+echo "build frontend"
+cd frontend
+npm run build
+
+cd ..
+git status
+cd deploy
