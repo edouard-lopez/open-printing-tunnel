ba135158b2482d17dd653db04d12eb736a105276
Édouard Lopez <contact@edouard-lopez.com>
2016-07-22 17:50:56 +0200

    add Flask restful

--- daemon/api/api.py
+++ daemon/api/api.py
@@ -1,13 +1,19 @@
+import subprocess
 from flask import Flask
+from flask_restful import reqparse, Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class Root(Resource):
+    def get(self):
+        return {
+            'output': str(subprocess.check_output(["ls"]))
+        }
 
+api.add_resource(Root, '/')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
-
