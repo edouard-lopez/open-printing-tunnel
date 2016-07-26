be1712eaa038b5f281c00814a8497bcee50e33ef
Édouard Lopez <contact@edouard-lopez.com>
2016-07-26 10:40:17 +0200

    fix typo

--- daemon/api/api.py
+++ daemon/api/api.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 
+
 class Mast():
     service = '/etc/init.d/mast'
     makefile = '/usr/sbin/mast-utils'
@@ -17,15 +18,15 @@ class Root(Resource):
             'output': str(subprocess.check_output(["ls"]))
         }
 
+
 class ListHosts(Resource):
     def get(self):
         cmd = [Mast.makefile, 'list-hosts']
         return str(subprocess.check_output(cmd))
 
 
-
 api.add_resource(Root, '/')
-api.add_resource(ListHosts, '/list-hosts/')g
+api.add_resource(ListHosts, '/list-hosts/')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
