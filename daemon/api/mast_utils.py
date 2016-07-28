d7a0fa0ccee8af56f3c7cc06e9284c42bdc494e8
Édouard Lopez <contact@edouard-lopez.com>
2016-07-29 00:38:51 +0200

    fix typo in add_host()

--- daemon/api/mast_utils.py
+++ daemon/api/mast_utils.py
@@ -34,7 +34,7 @@ def list_hosts():
 
 
 def add_host(name, hostname):
-    command = [makefile, 'add-host', 'NAME=' + name, 'REMOTE_HOST' + hostname]
+    command = [makefile, 'add-host', 'NAME=' + name, 'REMOTE_HOST=' + hostname]
     return shell.execute(command)
 
 
