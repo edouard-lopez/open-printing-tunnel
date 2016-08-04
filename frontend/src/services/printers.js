eaf707ffc38a66c80750fa851a351fc43e251d8e
Guillaume Vincent <guillaume@akema.fr>
2016-08-04 17:53:13 +0200

    import printer parsers

--- frontend/src/services/printers.js
+++ frontend/src/services/printers.js
@@ -42,5 +42,25 @@ export default {
 			.then(response => {
 				return response.data;
 			});
+	},
+	parseLine(line) {
+		if (line) {
+			const fields = line.split('\t');
+
+			return {
+				DESC: fields[0],
+				PRINTER: fields[1],
+				PORT: fields[2] || null
+			};
+		}
+	},
+	parsePrinters(clipboard) {
+		return clipboard.split('\n')
+			.filter(line => {
+				return line;
+			})
+			.map(line => {
+				return this.parseLine(line);
+			});
 	}
 };
---
+++ frontend/test/printers-parser.tests.js
@@ -0,0 +1,53 @@
+import test from 'ava';
+import printers from '../src/services/printers';
+
+test('should parse a line', t => {
+	const line = 'IMP RDC Couleur\t10.100.12.21';
+	const parsedLine = printers.parseLine(line);
+
+	t.deepEqual(parsedLine, {
+		DESC: 'IMP RDC Couleur',
+		PRINTER: '10.100.12.21',
+		PORT: null
+	});
+});
+
+test('should parse two lines', t => {
+	const clipboard = 'IMP RDC Couleur\t10.100.12.21\nHP2055 Sylvie\t10.100.12.25';
+	const parsedLines = printers.parsePrinters(clipboard);
+
+	t.deepEqual(parsedLines, [
+		{
+			DESC: 'IMP RDC Couleur',
+			PRINTER: '10.100.12.21',
+			PORT: null
+		},
+		{
+			DESC: 'HP2055 Sylvie',
+			PRINTER: '10.100.12.25',
+			PORT: null
+		}
+	]);
+});
+
+test('should parse port', t => {
+	const line = 'Copieur Salle de reunion\t10.100.12.24\t9102';
+	const parsedLine = printers.parseLine(line);
+
+	t.deepEqual(parsedLine, {
+		DESC: 'Copieur Salle de reunion',
+		PRINTER: '10.100.12.24',
+		PORT: '9102'
+	});
+});
+
+test('should not parse empty string', t => {
+	t.deepEqual(printers.parseLine(''), undefined);
+});
+
+test('should parse two printers', t => {
+	const clipboard = '\nIMP RDC Couleur\t10.100.12.21\n\nHP2055 Sylvie\t10.100.12.25';
+	const parsedLines = printers.parsePrinters(clipboard);
+
+	t.is(2, parsedLines.length);
+});
