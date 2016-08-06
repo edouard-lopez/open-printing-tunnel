84f6583ed71946c4a3ab23f00b514995781493de
Édouard Lopez <contact@edouard-lopez.com>
2016-08-07 01:47:24 +0200

    extract mutations to file

---
+++ frontend/src/store/mutations.js
@@ -0,0 +1,25 @@
+import optboxesService from '../services/optboxes.service';
+
+export default {
+	setOptboxes(state, optboxes) {
+		for (var optbox of optboxes) {
+			state.optboxes[optbox.id] = optbox;
+		}
+	},
+	// todo
+	// insertOptbox(state, optbox) {
+	// 	state.optboxes.push(optbox);
+	// },
+	removeOptbox(state, optbox) {
+		optboxesService.remove(state.optboxes, optbox);
+	},
+	setPrinters(state, optboxId, printers) {
+		console.log('setPrinters', optboxId, printers)
+		state.optboxes[optboxId].printers = printers || [];
+	}
+	//,
+	// todo
+	// removePrinter(state, printer) {
+	// 	printersService.remove(state.printers, printer);
+	// }
+};
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,22 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import optboxesService from '../services/optboxes.service';
+import mutations from './mutations';
+
 Vue.use(Vuex);
 
 const state = {
 	count: 0,
-	optboxes: [],
-	printers: []
+	optboxes: {},
 };
 
-const mutations = {
-	setOptboxes(state, optboxes) {
-		state.optboxes = optboxes;
-	},
-	removeOptbox(state, optbox) {
-		optboxesService.remove(state.optboxes, optbox);
-	}
-};
 
 export default new Vuex.Store({
 	state,
