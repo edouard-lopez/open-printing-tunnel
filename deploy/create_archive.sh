5c342b10708f7ea3b7e1001b82344b8c3474a69c
Édouard Lopez <contact@edouard-lopez.com>
2016-08-10 12:06:25 +0200

    pass hostname as 2nd argument, send error message to stderr

--- deploy/create_archive.sh
+++ deploy/create_archive.sh
@@ -5,9 +5,8 @@ cd ..
 LAST_TAG=$(git describe --abbrev=0 --tags)
 CURRENT_TAG=$(git describe --tags HEAD)
 
-if [ "$LAST_TAG" != "$CURRENT_TAG" ]
-then
-    echo "warning project is not tagged properly"
+if [ "$LAST_TAG" != "$CURRENT_TAG" ]; then
+    >&2 echo "warning project is not tagged properly"
 fi
 
 FILENAME="opt-$LAST_TAG.zip"
--- deploy/send_archive.sh
+++ deploy/send_archive.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if (( $# != 1 ))
-then
-  echo "Usage: source send_archive.sh opt-vX.X.X.zip"
-  return 1
+if (( $# < 1 )); then
+  >&2 echo "Usage: source send_archive.sh opt-vX.X.X.zip [hostname]"
+  exit 1
 fi
 
 FILENAME="$1"
+SERVER_IP="${2:-192.168.2.231}"
 echo "send archive on the server"
-scp -P 2222 $FILENAME coaxis@192.168.2.231:/home/coaxis
-scp -P 2222 deploy.sh coaxis@192.168.2.231:/home/coaxis
+scp -P 2222 $FILENAME coaxis@"$SERVER_IP":/home/coaxis
+scp -P 2222 deploy.sh coaxis@"$SERVER_IP":/home/coaxis
 rm $FILENAME
