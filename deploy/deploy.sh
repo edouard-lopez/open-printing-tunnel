9eaf2086353b354d9dcb3383355036907f48a87e
Édouard Lopez <contact@edouard-lopez.com>
2016-09-08 15:58:59 +0200

    add support to deploying specific tag
    # Conflicts:
    #	deploy/deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
+echo "Usage: ./deploy.sh [TAG [DEFAULT_INTERFACE]]"
 
-echo "Usage: ./deploy.sh [DEFAULT_INTERFACE]"
-
-DEFAULT_INTERFACE="${1:-ens192}"
-
+DEFAULT_INTERFACE="${2:-ens192}"
 export DEFAULT_INTERFACE
-
 export COMPOSE_HTTP_TIMEOUT=600
 
 # pull new images
 docker-compose pull
 docker pull docker.akema.fr:5000/coaxis/coaxisopt_daemon:latest
+docker_tags=(
+    'latest'
+    "$1"
+)
+
+function pull_images() {
+    declare -a tags=("${!1}")
+
+    docker-compose pull
+    for tag in ${tags[@]}; do
+        docker pull docker.akema.fr:5000/coaxis/coaxisopt_daemon:"$tag"
+    done
+}
+
+function clean_images() {
+    docker rmi $(docker images -f "dangling=true" -q)
+}
 
 # restart container
 docker-compose down
 docker-compose up -d
 docker rmi $(docker images -f "dangling=true" -q)
+
+function restart() {
+    docker-compose down
+    docker-compose up -d
+}
+
+# see http://stackoverflow.com/a/4017175/802365 and http://stackoverflow.com/a/6828383/802365
+pull_images docker_tags[@]
+restart
+clean_images
