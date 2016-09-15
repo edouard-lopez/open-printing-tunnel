0e7c118d7b759cb4f6d12eb0822ef5933f099c99
Édouard Lopez <contact@edouard-lopez.com>
2016-09-15 17:13:48 +0200

    remove useless complexity and show usage

--- deploy/tag_and_push.sh
+++ deploy/tag_and_push.sh
@@ -3,6 +3,12 @@ onexit(){ while caller $((n++)); do :; done; }
 trap onexit EXIT
 #set -x  # debug
 
+TAG="${1:=latest}"
+
+function usage() {
+    printf "Usage: ./tag_and_push.sh [tag]\n\n"
+}
+
 function build_images() {
     cd ..
     docker-compose build
@@ -14,25 +20,20 @@ function build_images() {
 # Tag all images
 function tag_images() {
     declare -a images=("${!1}")
-    declare -a tags=("${!2}")
+    tag="$2"
 
     for image in ${images[@]}; do
-        for tag in ${tags[@]}; do
-
-            docker tag "$image" docker.akema.fr:5000/coaxis/"$image":"$tag"
-        done
+        docker tag "$image" docker.akema.fr:5000/coaxis/"$image":"$tag"
     done
 }
 
 # Push all tagged images
 function push_images() {
     declare -a images=("${!1}")
-    declare -a tags=("${!2}")
+    tag="$2"
 
     for image in ${images[@]}; do
-        for tag in ${tags[@]}; do
-            docker push docker.akema.fr:5000/coaxis/"$image":"$tag"
-        done
+        docker push docker.akema.fr:5000/coaxis/"$image":"$tag"
     done
 }
 
@@ -43,11 +44,9 @@ docker_images=(
     'coaxisopt_frontend'
     'coaxisopt_backend'
 )
-docker_tags=(
-    'latest'
-    "$1"
-)
+
+usage
 build_images
 # see http://stackoverflow.com/a/4017175/802365 and http://stackoverflow.com/a/6828383/802365
-tag_images docker_images[@] docker_tags[@]
-push_images docker_images[@] docker_tags[@]
+tag_images docker_images[@] "$TAG"
+push_images docker_images[@] "$TAG"
