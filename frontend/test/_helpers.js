064df124372830bb675f27415400066c2d6625cd
Édouard Lopez <contact@edouard-lopez.com>
2017-03-10 13:55:14 +0100

    remove useless service and tests

--- frontend/test/_helpers.js
+++ frontend/test/_helpers.js
@@ -22,40 +22,6 @@ function storageMock() {
 }
 export {storageMock};
 
-const containersGetAll = {
-	count: 2,
-	next: null,
-	previous: null,
-	results: [
-		{
-			id: '049fed91-6880-4c08-8cb2-21e8579d4543',
-			description: '您的瀏覽器需要安裝Flash插件 ',
-			company: '0d8dbbf3-73f4-49ab-a550-22c1fca93439',
-			container_id: 'a5775b3cf95c465d5abcee59934c087cad9ca3d20bee266c44576b7b3d06ac1c',
-			infos: {
-				id: 'a5775b3cf95c465d5abcee59934c087cad9ca3d20bee266c44576b7b3d06ac1c',
-				status: 'running',
-				id: '/mast__e45b0231-251f-401d-b379-eb5875fc343b'
-			}
-		},
-		{
-			id: '0d8dbbf3-73f4-49ab-a550-22c1fca93439',
-			description: '為了更好瀏覽多媒體內容',
-			company: '049fed91-6880-4c08-8cb2-21e8579d4543',
-			container_id: 'a5775b3cf95c465d5abcee59934c087cad9ca3d20bee266c44576b7b3d06ac1c',
-			infos: {
-				id: 'a5775b3cf95c465d5abcee59934c087cad9ca3d20bee266c44576b7b3d06ac1c',
-				status: 'running',
-				id: '/mast__e45b0231-251f-401d-b379-eb5875fc343b'
-			}
-		}
-	]
-};
-export {containersGetAll};
-
-const containersGetOne = JSON.parse(JSON.stringify(containersGetAll.results[0]));
-export {containersGetOne};
-
 const sitesGetAll = {
 	results: [
 		{
@@ -77,23 +43,22 @@ export {sitesGetAll};
 const siteGetOne = JSON.parse(JSON.stringify(sitesGetAll.results[0]));
 export {siteGetOne};
 
-
 const printersGetAll = {
 	data: [
 		{
 			id: 0,
 			hostname: '1.2.3.4',
-			description: 'salle 1',
+			description: 'salle 1'
 		},
 		{
 			id: 4,
 			hostname: '1.2.3.5',
-			description: 'salle 2',
+			description: 'salle 2'
 		},
 		{
 			id: 3,
 			hostname: '1.2.3.6',
-			description: 'salle 3',
+			description: 'salle 3'
 		}
 	]
 };
--- frontend/test/containers.service.tests.js
+++ frontend/test/containers.service.tests.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import nock from 'nock';
 
 import containers from '../src/services/containers.service';
-import {storageMock, containersGetAll, containersGetOne} from './_helpers';
+import {storageMock} from './_helpers';
 
 containers.localStorage = storageMock();
 
@@ -38,51 +38,4 @@ test('should send requests with Authorization header updated', t => {
 	return containers.all().then(response => {
 		t.is(response.status, 200);
 	});
-});
-
-test('should get all containers with offset', t => {
-	nock('http://localhost/').get('/api/containers/').query(true).reply(200, {containers: containersGetAll});
-
-	return containers.all().then(response => {
-		t.is(response.status, 200);
-		t.is(response.data.containers.results.length, containersGetAll.count);
-	});
-});
-
-test('should get all containers with parameters', t => {
-	nock('http://localhost/').get('/api/containers/?limit=100&offset=0&search=query&ordering=-created')
-		.reply(200, {});
-
-	return containers.all(100, 0, 'query', '-created').then(response => {
-		t.is(response.status, 200);
-	});
-});
-
-test('should get a container', t => {
-	nock('http://localhost/').get('/api/containers/049fed91-6880-4c08-8cb2-21e8579d4543/').reply(200, containersGetOne);
-
-	return containers.get('049fed91-6880-4c08-8cb2-21e8579d4543').then(container => {
-		t.truthy(containersGetOne.company);
-		t.is(containersGetOne.company, container.company);
-	});
-});
-
-test('should update a container', t => {
-	const updatedContainer = JSON.parse(JSON.stringify(containersGetOne));
-	updatedContainer.description = 'new description';
-	nock('http://localhost/').put(`/api/containers/${updatedContainer.id}/`).reply(200, updatedContainer);
-
-	return containers.update(updatedContainer)
-		.then(container => {
-			t.is(container.description, 'new description');
-		});
-});
-
-test('should delete a container', t => {
-	nock('http://localhost/').delete(`/api/containers/${containersGetOne.id}/`).reply(200);
-
-	return containers.delete(containersGetOne)
-		.then(response => {
-			t.is(response, '');
-		});
 });
