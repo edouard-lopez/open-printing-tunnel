f525d7662607baf1c470debb260bb370398e1189
Édouard Lopez <contact@edouard-lopez.com>
2018-10-11 11:22:53 +0200

    implement write operation for ConfigEditor

--- daemon/api/config_editor.py
+++ daemon/api/config_editor.py
@@ -15,8 +15,22 @@ class ConfigEditor:
         with open(file_path) as file:
             for line in file:
                 lexer = shlex.shlex(line)
-                varname = lexer.get_token()
+                var_name = lexer.get_token()
                 if lexer.get_token() == '=':
                     value = ''.join(lexer)
-                    content.update({varname: value})
+                    content.update({var_name: value})
         return content
+
+    def update(self, file_path, data={}):
+        import in_place
+        if len(data) == 0:
+            return
+
+        with in_place.InPlace(file_path) as file:
+            for line in file:
+                for var_name, value in data.items():
+                    # print("looking for {} in {}".format(var_name, line))
+                    if line.startswith(var_name):
+                        file.write("{}={}\n".format(var_name, value))
+                    else:
+                        file.write(line)
--- daemon/api/tests/test_config_editor.py
+++ daemon/api/tests/test_config_editor.py
@@ -37,7 +37,7 @@ class ConfigEditorTestCase(unittest.TestCase):
             config_editor = ConfigEditor()
             content = config_editor.load(file_path=site_config.name)
             
-            self.assertDictEqual({'BAR': '"abc"', 'FOO': '123'}, content)
+            self.assertDictEqual(content, {'BAR': '"abc"', 'FOO': '123'})
 
     def test_config_editor_can_load_config_file(self):
         site_config = os.path.join(WORKING_DIR, '..', 'template')
@@ -46,3 +46,50 @@ class ConfigEditorTestCase(unittest.TestCase):
         content = config_editor.load(file_path=site_config)
         
         self.assertIn('UploadLimit', content)
+
+    def test_config_editor_update_is_idempotent(self):
+        with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as site_config:
+            site_config.write('UploadLimit=100')
+            site_config.close()
+
+            config_editor = ConfigEditor()
+            config_editor.update(file_path=site_config.name)
+            content = config_editor.load(file_path=site_config.name)
+            
+            self.assertDictEqual(content, {'UploadLimit': '100'})
+
+    def test_config_editor_update_write_new_value(self):
+        with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as site_config:
+            site_config.write('UploadLimit=100')
+            site_config.close()
+
+            config_editor = ConfigEditor()
+            config_editor.update(file_path=site_config.name, data={'UploadLimit': 80})
+            content = config_editor.load(file_path=site_config.name)
+            
+            self.assertDictEqual(content, {'UploadLimit': '80'})
+
+    def test_config_editor_update_preserve_other_values(self):
+        with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as site_config:
+            site_config.write('UploadLimit=100\nDownloadLimit=10000')
+            site_config.close()
+
+            config_editor = ConfigEditor()
+            config_editor.update(file_path=site_config.name, data={'UploadLimit': 80})
+            content = config_editor.load(file_path=site_config.name)
+            
+            self.assertDictEqual(content, {'UploadLimit': '80', 'DownloadLimit': '10000'})
+
+    def test_config_editor_update_play_nice_with_real_file(self):
+        site_config = os.path.join(WORKING_DIR, '..', 'template')
+        config_editor = ConfigEditor()
+        content = config_editor.load(file_path=site_config)
+        backup_value = content.get('UploadLimit')
+
+        config_editor.update(file_path=site_config, data={'UploadLimit': 80})
+        content = config_editor.load(file_path=site_config)
+        
+        self.assertEqual(content.get('UploadLimit'), '80')
+        self.assertEqual(content.get('DownloadLimit'), '10000')
+        # restore initial value
+        config_editor.update(file_path=site_config, data={'UploadLimit': backup_value})
