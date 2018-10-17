e70e61eb2a99cb254e1c1e570bc875a6360616a6
Édouard Lopez <contact@edouard-lopez.com>
2018-10-17 18:31:24 +0200

    test API PUT and DELETE /api/sites/<site_d>

--- daemon/api/tests/test_views.py
+++ daemon/api/tests/test_views.py
@@ -47,19 +47,64 @@ class TestAPIIntegrations(TestCase):
         self.assertEqual(response.status_code, 500)
 
     def test_POST_api_sites_endpoint_require_id_and_hostname(self):
-        response = self.app.post('/api/sites/', json={"id": "paris", "hostname": "10.0.1.8"})
+        response = self.app.post('/api/sites/', json={"id": "paris", "hostname": "0.0.0.0"})
 
         self.assertEqual(response.status_code, 201)
 
-    def test_GET_api_sites_endpoint(self):
-        self.app.post('/api/sites/', json={"id": "paris", "hostname": "10.0.1.8"})
+    def test_GET_api_sites_endpoint_fetch_newly_created_site(self):
+        self.app.post('/api/sites/', json={"id": "paris", "hostname": "0.0.0.0"})
 
         response = self.app.get('/api/sites/')
         json = response.get_json()
 
         self.assertIn('results', json)
         self.assertGreaterEqual(len(json['results']), 1)
-        self.assertIn('channels', json['results'][0])
-        self.assertIn('cmd', json)
-        self.assertGreaterEqual(len(json['cmd']), 1)
-        self.assertEqual(json['cmd']['exit_status'], True)
+        self.assertDictEqual(json['results'][0], {'channels': [], 'hostname': '0.0.0.0', 'id': 'paris'})
+
+    def test_PUT_api_site_endpoint_reject_require_action_attribute(self):
+        response = self.app.post('/api/sites/', json={})
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_PUT_api_site_endpoint_reject_invalid_action(self):
+        response = self.app.post('/api/sites/', json={'action': 'invalid'})
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_PUT_api_site_endpoint_start_site(self):
+        self.app.post('/api/sites/', json={"id": "paris", "hostname": "0.0.0.0"})
+
+        response = self.app.put('/api/sites/paris/', json={'action': 'start'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('status', response.get_json()['results'])
+
+    def test_PUT_api_site_endpoint_stop_site(self):
+        self.app.post('/api/sites/', json={"id": "paris", "hostname": "0.0.0.0"})
+
+        response = self.app.put('/api/sites/paris/', json={'action': 'stop'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('status', response.get_json()['results'])
+
+    def test_PUT_api_site_endpoint_check_status_site(self):
+        self.app.post('/api/sites/', json={"id": "paris", "hostname": "0.0.0.0"})
+
+        response = self.app.put('/api/sites/paris/', json={'action': 'status'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('state', response.get_json()['results'])
+
+    def test_PUT_api_site_endpoint_restart_site(self):
+        self.app.post('/api/sites/', json={"id": "paris", "hostname": "0.0.0.0"})
+
+        response = self.app.put('/api/sites/paris/', json={'action': 'restart'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('status', response.get_json()['results'])
+
+    def test_DELETE_api_site_endpoint(self):
+        self.app.post('/api/sites/', json={"id": "paris", "hostname": "0.0.0.0"})
+
+        response = self.app.delete('/api/sites/paris/')
+
+        self.assertEqual(response.status_code, 200)
