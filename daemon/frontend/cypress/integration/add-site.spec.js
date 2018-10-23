d2dbfa5854f2d379ba070c833583b17438c0e886
Édouard Lopez <contact@edouard-lopez.com>
2018-10-24 00:33:17 +0200

    prettify

--- daemon/frontend/cypress/integration/add-site.spec.js
+++ daemon/frontend/cypress/integration/add-site.spec.js
@@ -1,48 +1,56 @@
 describe('Add site', function() {
-    beforeEach(function(){
-        cy.server()
-        cy.route('/api/sites/', {}).as('getAllSites')
-        cy.visit('/').closeSplash()
-        cy.get('#notie-alert-outer').click()
-    })
-    
-    it('open dialog to add site', function() {
-        cy.get('[data-e2e=add-site]').click()
+	beforeEach(function() {
+		cy.server();
+		cy.route('/api/sites/', {}).as('getAllSites');
+		cy.visit('/').closeSplash();
+		cy.get('#notie-alert-outer').click();
+	});
 
-        cy.get('[data-e2e=add-site-modal]').should('have.class', 'in')
-    })
+	it('open dialog to add site', function() {
+		cy.get('[data-e2e=add-site]').click();
 
-    it('close new site dialog', function() {
-        cy.get('[data-e2e=add-site]').click()
-        cy.get('[data-e2e=add-site-modal] .modal-header [data-dismiss]').click()
-        cy.get('[data-e2e=add-site-modal]').should('not.have.class', 'in')
+		cy.get('[data-e2e=add-site-modal]').should('have.class', 'in');
+	});
 
-        cy.get('[data-e2e=add-site]').click()
-        cy.get('[data-e2e=add-site-modal] .modal-footer [data-dismiss]').click()
-        cy.get('[data-e2e=add-site-modal]').should('not.have.class', 'in')
-    })
+	it('close new site dialog', function() {
+		cy.get('[data-e2e=add-site]').click();
+		cy.get('[data-e2e=add-site-modal] .modal-header [data-dismiss]').click();
+		cy.get('[data-e2e=add-site-modal]').should('not.have.class', 'in');
 
-    it('fill new site dialog', function() {
-        cy.route({
-            method: 'POST',
-            url: '/api/sites/',
-            response: {},
-            delay: 100
-          }).as('newSite')
+		cy.get('[data-e2e=add-site]').click();
+		cy.get('[data-e2e=add-site-modal] .modal-footer [data-dismiss]').click();
+		cy.get('[data-e2e=add-site-modal]').should('not.have.class', 'in');
+	});
 
-        cy.get('[data-e2e=add-site]').click()
-        cy.get('[data-e2e="add-site:name"]').clear().type('test-site')
-        cy.get('[data-e2e="add-site:hostname"]').clear().type('0.0.0.0')
-          
-        cy.get('[data-e2e="add-site:submit"]').click().contains('Création en cours')
-    })
+	it('fill new site dialog', function() {
+		cy.route({
+			method: 'POST',
+			url: '/api/sites/',
+			response: {},
+			delay: 100
+		}).as('newSite');
 
-    it('create a new site', function() {
-        cy.route('POST', '/api/sites/', {})
-        cy.route('/api/sites/', 'fixture:new-site')
+		cy.get('[data-e2e=add-site]').click();
+		cy.get('[data-e2e="add-site:name"]')
+			.clear()
+			.type('test-site');
+		cy.get('[data-e2e="add-site:hostname"]')
+			.clear()
+			.type('0.0.0.0');
 
-        cy.addNewSite('test-site', '0.0.0.0')
-        cy.get('[data-e2e="no-site-available"]').should('not.be.visible')
-        cy.get('[data-e2e="sites"]').children().should('have.length', 1)
-    })    
-})
+		cy.get('[data-e2e="add-site:submit"]')
+			.click()
+			.contains('Création en cours');
+	});
+
+	it('create a new site', function() {
+		cy.route('POST', '/api/sites/', {});
+		cy.route('/api/sites/', 'fixture:new-site');
+
+		cy.addNewSite('test-site', '0.0.0.0');
+		cy.get('[data-e2e="no-site-available"]').should('not.be.visible');
+		cy.get('[data-e2e="sites"]')
+			.children()
+			.should('have.length', 1);
+	});
+});
--- daemon/frontend/cypress/integration/splash.spec.js
+++ daemon/frontend/cypress/integration/splash.spec.js
@@ -1,19 +1,25 @@
 describe('Splash warning', function() {
-    beforeEach(function(){
-        cy.visit('/')
-    })
+  beforeEach(function() {
+    cy.visit('/');
+  });
 
-    it('is visible', function() {
-        cy.get('#splash').should('be.visible')
-        cy.get('#splash .modal-header button').should('be.visible')
-        cy.get('#splash .modal-footer button').contains('Accepter').and('be.visible')
-    })
-    
-    it('can be closed by x', function() {
-        cy.get('#splash .modal-header button').click().should('not.be.visible')
-    })
-    
-    it('can be closed by accept button', function() {
-        cy.get('#splash .modal-footer button').click().should('not.be.visible')
-    })
-})
+  it('is visible', function() {
+    cy.get('#splash').should('be.visible');
+    cy.get('#splash .modal-header button').should('be.visible');
+    cy.get('#splash .modal-footer button')
+      .contains('Accepter')
+      .and('be.visible');
+  });
+
+  it('can be closed by x', function() {
+    cy.get('#splash .modal-header button')
+      .click()
+      .should('not.be.visible');
+  });
+
+  it('can be closed by accept button', function() {
+    cy.get('#splash .modal-footer button')
+      .click()
+      .should('not.be.visible');
+  });
+});
