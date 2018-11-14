5a3bed5fb19a9d8c35a6013198f9a4f7af8fbf2d
Édouard Lopez <contact@edouard-lopez.com>
2018-11-14 23:53:42 +0100

    quote bash variables

--- deploy/send-scripts.sh
+++ deploy/send-scripts.sh
@@ -9,10 +9,8 @@ cd ..
 HOST="${1:-coaxis@optbox-forward}"
 SSH_PORT="${2:-2222}"
 
-ssh -p $SSH_PORT $HOST 'mkdir -p ~/coaxisopt'
-scp -P $SSH_PORT docker-compose.prod.yml "$HOST":~/
-scp -P $SSH_PORT deploy/deploy.sh "$HOST":~/coaxisopt
-ssh -p $SSH_PORT $HOST 'mv docker-compose.prod.yml ~/coaxisopt/docker-compose.yml'
-scp -P $SSH_PORT deploy/start-opt-tunnels.sh "$HOST":~/coaxisopt
-
-cd deploy
+ssh -p "$SSH_PORT" "$HOST" 'mkdir -p ~/coaxisopt'
+scp -P "$SSH_PORT" docker-compose.prod.yml "$HOST":~/
+scp -P "$SSH_PORT" deploy/deploy.sh "$HOST":~/coaxisopt
+ssh -p "$SSH_PORT" "$HOST" 'mv docker-compose.prod.yml ~/coaxisopt/docker-compose.yml'
+scp -P "$SSH_PORT" deploy/start-opt-tunnels.sh "$HOST":~/coaxisopt
